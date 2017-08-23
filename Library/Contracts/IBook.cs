5daf308af22063b297128ac0cc2873beacde7646
Pratik Kumar <pkumar@tavisca.com>
2017-08-23 13:20:50 +0530

    Added book and genre

---
+++ Library/Contracts/IBook.cs
@@ -0,0 +1,14 @@
+﻿using Library.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Contracts
+{
+    public interface IBook
+    {
+        int Id { get; }
+        string Name { get; }
+        Genre Genre { get; }
+    }
+}
---
+++ Library/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using Library.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Constants;
+
+namespace Library.Models
+{
+    public class Book : IBook
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public Genre Genre { get; }
+
+        public Book(int Id, string Name, Genre Genre)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Genre = Genre;
+        }
+    }
+}

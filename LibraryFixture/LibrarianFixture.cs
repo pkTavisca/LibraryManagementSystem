e7257a7750a48b9703fa38368caf03525fe84edc
Pratik Kumar <pkumar@tavisca.com>
2017-08-23 14:15:03 +0530

    Librarian Test

---
+++ LibraryFixture/LibrarianFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Library.Contracts;
+using Library.Constants;
+using Library.Models;
+using Library;
+using Xunit;
+
+namespace LibraryFixture
+{
+    public class LibrarianFixture
+    {
+        [Fact]
+        public void Issuing_a_book()
+        {
+            List<IBook> books = new List<IBook>();
+            var book = new Book(1, "qw", Genre.Adventure);
+            books.Add(book);
+            BookShelf shelf = new BookShelf(books);
+            Librarian librarian = new Librarian(shelf);
+
+            librarian.IssueBook(book);
+            Assert.Equal(book.ToString(), librarian.GetAllIssuedBooks().Trim('\n'));
+        }
+    }
+}
